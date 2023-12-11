130f909f17c7f2b80e72ab32666b9a554d1caf38
Mr-G-D <daredinesh00@gmail.com>
2023-12-11 17:24:42 -0500

    Array Product Except Self :pencil2:

---
+++ Extras/arrayProductExceptSelf.java
@@ -0,0 +1,23 @@
+    public int[] productExceptSelf(int[] nums) {
+
+        int[] result = new int[nums.length];
+        int[] pre = new int[nums.length];
+        int[] post = new int[nums.length];
+        pre[0] = 1;
+        int size = nums.length;
+        post[size-1] = 1;
+        
+        for(int i=0; i<size-1; i++){
+            pre[i+1] = pre[i]*nums[i];
+        }
+        
+        for(int i=size-1; i>0; i--){
+            post[i-1] = post[i]*nums[i];
+        }
+
+        for(int i=0; i<=size-1; i++){
+            result[i] = pre[i]*post[i];
+        }
+
+        return result;
+    }
