5cce0eb14a02e8079b3eae425ef081cdf2c7cf74
Mr-G-D <daredinesh00@gmail.com>
2025-07-12 18:23:24 -0400

    Duplicate Elements

--- Extras/Arrays/duplicateElements.java
+++ Extras/Arrays/duplicateElements.java
@@ -1,16 +1,16 @@
 public class duplicateElements {
 
-//        public static <ListNode> ListNode main(ListNode head) {
-//            ListNode node = head;
-//            while(node != null && node.next != null){
-//                if(node.val == node.next.val){
-//                    node.next = node.next.next;
-//                }else{
-//                    node = node.next;
-//                }
-//
-//            }
-//
-//            return head;
-//        }
+        public static <ListNode> ListNode duplicate(ListNode head) {
+            ListNode node = head;
+            while(node != null && node.next != null){
+                if(node.val == node.next.val){
+                    node.next = node.next.next;
+                }else{
+                    node = node.next;
+                }
+
+            }
+
+            return head;
+        }
 }
